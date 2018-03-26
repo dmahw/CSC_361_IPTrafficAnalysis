64b35650bc99f77c471b0203719c7a4df7d84203
dmahw <28464255+dmahw@users.noreply.github.com>
2018-03-26 00:45:08 -0700

    Some work has been done

--- IPTrafficAnalysis.py
+++ IPTrafficAnalysis.py
@@ -1,40 +1,117 @@
+import sys, dpkt, socket
+from dpkt.compat import compat_ord
+
+TYPE = -1
+TYPE_LINUX = 0
+TYPE_WINDOW = 1
+FIRST_PACKET = 0
+
+class Trace:
+    src_ip = 0
+    dst_ip = 0
+    rtr_ip = []
+    proto = []
+    num_frags = 0
+    off_frags = 0
+    src_pkts = []
+
+def mac_addr(address):      #Refer to Reference                     #Used to convert binary to mac addresses
+    return ":".join("%02x" % compat_ord(b) for b in address)
+
+def ipAddress(inet):      #Refer to Reference                     #Used to convert binary to Ip Addresses
+    return socket.inet_ntop(socket.AF_INET, inet)
+
+def linux_workflow(eth):
+    global trace
+    ip = eth.data
+    if ip.p == dpkt.ip.IP_PROTO_UDP:
+        udp = ip.data
+        print("UDP Packet")
+        trace.src_pkts.append(eth)
+        
+        print("Time to live: " + str(ip.ttl))
+        
+        # print(trace.src_pkts[0].data.ttl)
+        # print(trace.src_pkts[0].data.data.sport)
+    else:
+        return 0
+
+def window_workflow(eth):
+    return 0
+
+def get_first_packet(eth):
+    global FIRST_PACKET
+    global trace
+    global TYPE
+
+    ip = eth.data
+    if ip.p == dpkt.ip.IP_PROTO_UDP:
+        udp = ip.data
+        if FIRST_PACKET == 0:
+            if ip.ttl == 1:
+                FIRST_PACKET = 1
+                TYPE = TYPE_LINUX
+                print("Traceroute is Linux Type")
+                trace.src_pkts.append(eth)
+                
+                trace.src_ip = ipAddress(ip.src)
+                trace.dst_ip = ipAddress(ip.dst)
+                print("Source IP: " + trace.src_ip)
+                print("Destination IP: " + trace.dst_ip)
+                print("Time to live: " + str(ip.ttl))
+                print("UDP Checksum: " + str(udp.sum))
+                
+                # print(trace.src_pkts[0].data.ttl)
+                # print(trace.src_pkts[0].data.data.sport)
+                return 1
+            else:
+                return 0
+
+    elif ip.p == dpkt.ip.IP_PROTO_ICMP:
+        icmp = ip.data
+        if FIRST_PACKET == 0:
+            if ip.ttl == 1:
+                FIRST_PACKET = 1
+                TYPE = TYPE_WINDOW
+                print("Traceroute is Windows Type")
+            else:
+                return 0
+    return 0
+
+
 def main():
-    traceFileName = sys.argv[1]                         #name of file to read from
+    traceFileName = sys.argv[1]
 
-    traceFile = open(traceFileName, "rb")               #open the file to read in binary
-    tracePcap = dpkt.pcap.Reader(traceFile)             #use a reader to parse
+    traceFile = open(traceFileName, 'rb')
+    tracePcap = dpkt.pcapng.Reader(traceFile)
 
-    stats = Statistics()                                
-    connections = Connections()
     count = 0
 
-    for timeStamp, buf in tracePcap:                    #Refer to reference. Parts of the referenced code has been deleted or modified.
+    for timeStamp, buf in tracePcap:
+        count = count + 1
+        if count >= 19:
+            break
+        print("\n********** Packet " + str(count) + " **********")
+
         eth = dpkt.ethernet.Ethernet(buf)
-        ip = eth.data                                   #IP Header
-        tcp = ip.data                                   #TCP Header
-
-        packet = Packet()                               #Storing various values into a packet class
-        packet.srcMac = mac_addr(eth.src)
-        packet.dstMac = mac_addr(eth.dst)
         
-        packet.srcIP = inet_to_str(ip.src)
-        packet.dstIP = inet_to_str(ip.dst)
-        packet.IPLen = ip.len
-        packet.id = ip.id
-        
-        packet.seq = tcp.seq
-        packet.ack = tcp.ack
-        packet.windowSize = tcp.win
-        packet.flagsBin = tcp.flags
-        packet.srcPort = tcp.sport
-        packet.dstPort = tcp.dport
-        packet.time = timeStamp
-        packet = binToFlags(packet)
+        if eth.type != dpkt.ethernet.ETH_TYPE_IP:
+            continue
 
-        analyzePacket(stats, connections, packet)       #For each packet, analyze
-        del packet
-    
-    finalStatCheck(stats, connections)
-    printFinal(stats, connections)
+        if FIRST_PACKET == 0:
+            get_first_packet(eth)     
+        elif TYPE == TYPE_LINUX:
+            linux_workflow(eth)
+        elif TYPE == TYPE_WINDOW:
+            window_workflow(eth)   
 
+        # src_ip = ipAddress(ip.src)
+        # dst_ip = ipAddress(ip.dst)
+        # print("\nPacket:" + str(count))
+        # print(src_ip)
+        # print(dst_ip)
+        # print(ip.ttl)
+
+
+trace = Trace()
 main()
